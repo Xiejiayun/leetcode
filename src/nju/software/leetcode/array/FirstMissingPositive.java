6745e5d0a1f2e58c47cfbc0ebb8d069c3fef73dc
Xiejiayun <furnace09@gmail.com>
2015-06-16 14:56:24 +0800

    Update FirstMissingPositive.java

--- src/nju/software/leetcode/array/FirstMissingPositive.java
+++ src/nju/software/leetcode/array/FirstMissingPositive.java
@@ -11,40 +11,40 @@ package nju.software.leetcode.array;
 public class FirstMissingPositive {
 
 	public static void main(String[] args) {
-
+	
 	}
 	
 	/*
 	 * given an array A, return the first missing positive 
 	 */
-    public int firstMissingPositive(int[] A) {
-        if (A == null)
-            return 1;
-        int N = A.length;
-        for (int i=0; i<N; i++) {
-            if (A[i] == i+1)
-                continue;  
-            if (A[i] != i+1 && A[i] > 0 && A[i] <= N) {
-            	if (A[i] == A[A[i]-1])
-            		continue;
-            	exchange(A, i, A[i]-1);
-            	 i--;
-            }
-        }
-        for (int i=0; i<N; i++) {
-            if (A[i] != i+1)
-                return i+1;
-        }
-        return N+1;
-    }
-    
-    /*
-     * exchange two integers in an array A at position i and j
-     */
-    public void exchange(int[] A, int i, int j) {
-        int tmp = A[i];
-        A[i] = A[j];
-        A[j] = tmp;
-    }
+	public int firstMissingPositive(int[] A) {
+	if (A == null)
+	    return 1;
+	int N = A.length;
+	for (int i=0; i<N; i++) {
+	    if (A[i] == i+1)
+	        continue;  
+	    if (A[i] != i+1 && A[i] > 0 && A[i] <= N) {
+	    	if (A[i] == A[A[i]-1])
+	    		continue;
+	    	exchange(A, i, A[i]-1);
+	    	 i--;
+	    }
+	}
+	for (int i=0; i<N; i++) {
+	    if (A[i] != i+1)
+	        return i+1;
+	}
+	return N+1;
+	}
+
+	/*
+	* exchange two integers in an array A at position i and j
+	*/
+	public void exchange(int[] A, int i, int j) {
+	    int tmp = A[i];
+	    A[i] = A[j];
+	    A[j] = tmp;
+	}
 
 }
